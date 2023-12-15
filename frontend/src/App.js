ce90cbe530f14d49137b0373ce2dc804171fc538
Bakr-19 <141832833+Bakr-19@users.noreply.github.com>
2023-12-15 09:50:47 +0000

    Newest

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import "./App.css";
+import StatsPage from "./Pages/statsPage"
+import NavBar from "./Compoents/navBar"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route
+
+          path='/'
+          element={<StatsPage />}
+
+
+        />
+
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/Compoents/navBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../App.css"
+
+
+const NavBar = () => {
+  return (
+    <div className='Container-Nav'>
+
+      <div className='header'>
+        <p>hello</p>
+
+      </div>
+
+
+
+    </div>
+  )
+}
+
+export default NavBar
