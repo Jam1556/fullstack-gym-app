7a6695c3045fc2e5b8d9c2d6ee1f96b8f7e0bced
Alex Winstanley <ajgwinstanley@hotmail.com>
2023-12-19 15:56:47 +0000

    Update About.js

--- frontend/src/Pages/About.js
+++ frontend/src/Pages/About.js
@@ -12,6 +12,18 @@ const About = () => {
     <p> I put forward a couple of design ideas for the overall app, as well as creating the functionality for the Workout page. I created the ADD button which conditionally renders some input bars and a submit button, and when that button is pressed it triggers a POST api fetch request, sending your workout information to the server. 
         There is also a GET request implemented in this button to show your recent workouts in the Workout Card I created on the left side of the screen. </p>
         </div>
+        <h2>Baker</h2>
+        <div className="about-container">
+            <p></p>
+        </div>
+        <h2>Jamie</h2>
+        <div className="about-container">
+            <p></p>
+        </div>
+<h2>Mubarik</h2>
+<div className="about-container">
+    <p></p>
+</div>
     </>
     )
 }
