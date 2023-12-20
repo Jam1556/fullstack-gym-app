266ae0f785fb5e26fe1494dc3496053693c86355
mubarik04 <mubbylub@gmail.com>
2023-12-20 09:23:45 +0000

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,26 @@
 import './App.css';
 
+const session = require('express-session');
+
+app.use(session({
+  secret: 'your secret key',
+  resave: false,
+  saveUninitialized: false,
+  // additional configurations
+}));
+
+const logout = async () => {
+  try {
+    const response = await fetch('/logout', { method: 'POST' });
+    if (!response.ok) {
+      throw new Error('Logout failed');
+    }
+    // Update state or redirect user
+  } catch (error) {
+    console.error('Logout error:', error);
+  }
+};
+
 function App() {
   return (
     <div className="App">
---
+++ frontend/src/Pages/LoginPage.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import "../Login.css"
+import { useState } from 'react'
+import { useLogin } from '../Context/useLogin'
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const {login, error, isLoading} = useLogin()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    await login(email, password)
+  }
+  const handleLogout = async () => {
+    // Call logout API or clear session/token here
+    console.log('Logout clicked'); // Placeholder action
+  }
+
+  return (
+
+    <div className='login-container'>
+
+    {/* form container code  */}
+      <form className="login" onSubmit={handleSubmit}>
+      <h3>Log In</h3>
+      
+      <label>Email address:</label>
+      <input 
+        type="email" 
+        onChange={(e) => setEmail(e.target.value)} 
+        value={email} 
+      />
+      <label>Password:</label>
+      <input 
+        type="password" 
+        onChange={(e) => setPassword(e.target.value)} 
+        value={password} 
+      />
+
+      <button disabled={isLoading}>Log in</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+
+
+    {/* A lot of divs */}
+      <div className='panel'>
+
+        <p className='text'>Sign up</p>
+        <div className='inputs'>
+          <div className='input'>
+            <i class="bi bi-person-fill"></i>
+            <input type='text' placeholder='Name' />
+
+          </div>
+          <div className='input'>
+            <i class="bi bi-envelope-at-fill"></i>
+            <input type='email' placeholder='Email' />
+
+          </div>
+          <div className='input'>
+            <i class="bi bi-lock-fill"></i>
+            <input type='password' placeholder='Password' />
+
+          </div>
+          <div className='submit-container'>
+            <div className='submit-signup'>Sign Up</div>
+            <div className='submit-login'>Login</div>
+          </div>
+
+
+        </div>
+
+
+      </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default LoginPage
